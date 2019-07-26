cba6a0f631112ad8e32a9d21b884f02af722cef9
ebisLab <ebaiden@techie.com>
2019-07-26 04:01:49 -0400

    edit and go back buttons

--- team-builder/src/components/Members.js
+++ team-builder/src/components/Members.js
@@ -3,11 +3,58 @@ import React, {useState} from 'react';
 function Members(props){
 
     console.log('member props', props)
-    // const [isEditing, setIsEditing] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [input, setInput]= useState({
+        name: props.member.name,
+        email: props.member.email,
+        role: props.member.role
+      //   role1:"",
+      //   role2: "",
+      //   role3:""
+    });
+  // console.log('props', props)
+    const handleiInput = e =>{
+        console.log('handle input Fire');
+      //   console.log('Value', e.target.value);
+      //   console.log('Name', e.target.name);
+        setInput({ ...input, [e.target.name] : e.target.value});
+  
+    };
 
-    // const handleEdit = e => {
-    //     setIsEditing(!isEditing)
-    // }
+    const handleEdit = e => {
+        // preventDefault()
+        setIsEditing(!isEditing)
+    }
+
+console.log('edit', isEditing)
+if(isEditing){
+    return(
+        <div className="App">
+      <h1>Add a member to</h1>
+      <p>The List</p>
+      <form>
+        <label htmlFor="name">
+          Name:<input type="text" value ={input.name} name="name" onChange={handleiInput} />
+        </label>
+        <label htmlFor="email">
+          Email:<input type="text" value ={input.email} name="email" onChange={handleiInput} />
+        </label>
+       <p>
+           <label htmlFor="role">
+          Role:
+          <input type="text" value ={input.role} name="role" onChange={handleiInput} />
+
+
+        </label></p>
+        <p><button>Add Member</button>
+        <button onClick={handleEdit}>Go Back</button></p>
+
+      </form>
+    </div>
+    )
+}
+
+
     return (
         <div>
             <h2>Member:</h2>
@@ -16,7 +63,7 @@ function Members(props){
             <h2>Email: {props.member.email}</h2>
             <h2>Role: {props.member.role}</h2>
 
-            <button /*onClick={handleEdit} */>Edit</button>
+            <button onClick={handleEdit}>Edit</button>
         </div>
     )
 }
