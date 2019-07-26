dd213e4fd259860a68e52678690c3625432265a4
ebisLab <ebaiden@techie.com>
2019-07-26 03:44:41 -0400

    members now print to page from members components

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -14,8 +14,7 @@ function App() {
   return (
     <div className="App">
       <Form addMember={addMember} />
-      {members.map(member => console.log('map', member))}
-      {/* < Members /> */}
+      {members.map(member => < Members member={member} />  )}
     </div>
   );
 }
--- team-builder/src/components/Members.js
+++ team-builder/src/components/Members.js
@@ -1,20 +1,22 @@
 import React, {useState} from 'react';
 
 function Members(props){
-    const [isEditing, setIsEditing] = useState(false);
 
-    const handleEdit = e => {
-        setIsEditing(!isEditing)
-    }
+    console.log('member props', props)
+    // const [isEditing, setIsEditing] = useState(false);
+
+    // const handleEdit = e => {
+    //     setIsEditing(!isEditing)
+    // }
     return (
         <div>
             <h2>Member:</h2>
             <p>{props.member.name}</p>
-            <h2>Name:</h2>
-            <h2>Email:</h2>
-            <h2>Role</h2>
+            <h2>Name: {props.member.name}</h2>
+            <h2>Email: {props.member.email}</h2>
+            <h2>Role: {props.member.role}</h2>
 
-            <button onClick={handleEdit}>Edit</button>
+            <button /*onClick={handleEdit} */>Edit</button>
         </div>
     )
 }
